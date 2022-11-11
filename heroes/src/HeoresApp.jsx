33e0f09dcdc2336c10791451b4b737830f366406
GeraSaucedo <jegsg@hotmail.es>
2022-11-11 15:33:40 -0600

    heroes spa added

---
+++ heroes/src/HeoresApp.jsx
@@ -0,0 +1,8 @@
+
+export const HeoresApp = () => {
+  return (
+    <>
+        <h1>Heroes App</h1>
+    </>
+  )
+}
---
+++ heroes/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HeoresApp } from './HeoresApp'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeoresApp />
+  </React.StrictMode>
+)
