7bb1043ff562365925379059ac7a867688caed77
Gerardo Saucedo <gerasaucedo.dev@gmail>
2022-11-16 14:14:01 -0600

    public and private routes

--- heroes/src/auth/pages/LoginPage.jsx
+++ heroes/src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
     login('Gerardo Saucedo');
 
-    navigate('/', {
+    navigate(lastPath, {
       replace: true
     });
   }
