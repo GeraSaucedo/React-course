d6d1cc4938acc97b7e2d7f09c1b786bb32d279b6
Gerardo Saucedo <gerasaucedo.dev@gmail>
2022-11-16 13:50:21 -0600

    user login and logout feature added

--- heroes/src/HeoresApp.jsx
+++ heroes/src/HeoresApp.jsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "./auth"
 import { AppRouter } from "./router/AppRouter"
 
 
 export const HeoresApp = () => {
   return (
-    <>
+    <AuthProvider>
         <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
