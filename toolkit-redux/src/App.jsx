7c00ccee2481ac82d9eb68b7a63775f1a6aa74a6
Gerardo Saucedo <gerasaucedo.dev@gmail>
2022-11-18 13:56:24 -0600

    redux toolkit

---
+++ toolkit-redux/src/App.jsx
@@ -0,0 +1,34 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, incrementBy } from "./store/slices/counter";
+
+function App() {
+  
+  const { counter } = useSelector( state => state.counter );
+  const dispatch = useDispatch();
+
+
+  return (
+    <div className="App">
+      <div>
+             <img src="/vite.svg" className="logo" alt="Vite logo" />
+                <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+      <h1>count is {counter}</h1>
+      <div className="card">
+        <button onClick={() => dispatch( increment() ) }>
+            Increment
+        </button>
+        <button onClick={() => dispatch( decrement() ) }>
+            Decrement
+        </button>
+        <button onClick={() => dispatch( incrementBy(2) ) }>
+            Increment by 2
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
