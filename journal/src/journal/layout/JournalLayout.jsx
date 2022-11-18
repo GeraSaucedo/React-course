6507d602390b0506966c07ff6a7f0598b18c1d89
Gerardo Saucedo <gerasaucedo.dev@gmail>
2022-11-18 12:26:58 -0600

    journal structure created

--- journal/src/journal/layout/JournalLayout.jsx
+++ journal/src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,8 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
@@ -9,13 +10,13 @@ export const JournalLayout = ({ children }) => {
 
         <NavBar drawerWidth={drawerWidth} />
 
-        {/* SideBar */}
+        <SideBar drawerWidth={drawerWidth} />
 
         <Box 
             component='main'
             sx={{ flexGrow: 1, p:3 }}
         >
-            {/* ToolBar */}
+            <Toolbar />
             { children }
         </Box>
 
